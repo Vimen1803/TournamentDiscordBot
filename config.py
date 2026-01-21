75991e8b8d2f48fb872a3affd9c6f6593f04ec0a
Victor Menjon <111061149+Vimen1803@users.noreply.github.com>
2026-01-21 02:50:06 +0100

    Remove redundant lines from config.py

--- config.py
+++ config.py
@@ -1,7 +1,5 @@
 #BASES DE DATOS
 URL_BASE_1: str = ""
-URL_BASE_2: str = ""
-URL_BASE_3: str = ""
 
 #TOKEN
 BOT: str = ""
@@ -11,5 +9,5 @@ BOT_LINK: str = ""
 #CANALES
 ERROR_CHANNEL: int = 0
 LOG_CHANNEL: int = 0
-
 BUG_CHANNEL: int = 0
+
