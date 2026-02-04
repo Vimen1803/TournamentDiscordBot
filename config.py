cc0a5acd7b6046cd576c248caa6bbffccdff0823
Victor Menjon <111061149+Vimen1803@users.noreply.github.com>
2026-02-04 22:46:23 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,15 +1,22 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 #BASES DE DATOS
-URL_BASE_1: str = ""
+URL_BASE_1: str = os.getenv("URL_BASE_1")
 
 #TOKEN
-BOT: str = ""
-PREFIX: str = ","
-BOT_LINK: str = ""
+BOT: str = os.getenv("DISCORD_BOT_TOKEN")
+PREFIX: str = os.getenv("BOT_PREFIX")
+BOT_LINK: str = os.getenv("BOT_LINK")
 
 #CANALES
-ERROR_CHANNEL: int = 0
-LOG_CHANNEL: int = 0
-BUG_CHANNEL: int = 0
+ERROR_CHANNEL: int = int(os.getenv("ERROR_CHANNEL_ID"))
+LOG_CHANNEL: int = int(os.getenv("LOG_CHANNEL_ID"))
+BUG_CHANNEL: int = int(os.getenv("BUG_CHANNEL_ID"))
+SERVER_LOG_CHANNEL: int = int(os.getenv("SERVER_LOG_CHANNEL_ID"))
 
 #DOCUMENTACION
-DOC_URL: str = ""
+DOC_URL: str = os.getenv("DOC_URL")
+OWNER = [int(os.getenv("OWNER_ID"))]
